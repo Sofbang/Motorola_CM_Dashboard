02f7f137ac335dde59a68970f1be52671b7ea551
vishalsehgalsofbang <45191802+vishalsehgalsofbang@users.noreply.github.com>
2019-02-07 15:18:56 +0530

    updated children elements in app side bar

--- Motorola-CM-Dashboard/src/app/templates/app-sidebar/app-sidebar.component.ts
+++ Motorola-CM-Dashboard/src/app/templates/app-sidebar/app-sidebar.component.ts
@@ -9,7 +9,19 @@ export class AppSidebarComponent implements OnInit {
 
   constructor() { }
 
+  // side bar events for close and open created by vishal as on 7/2/2019
+  public openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+  public closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+  }
+
   ngOnInit() {
+    
   }
 
 }
