4c2de2eea4cb51865810efb7e69e180b78e1bcf3
vishalsehgalsofbang <45191802+vishalsehgalsofbang@users.noreply.github.com>
2019-02-08 17:49:08 +0530

    Complete till graphs working

--- Motorola-CM-Dashboard/src/app/dashboard/dashboard.module.ts
+++ Motorola-CM-Dashboard/src/app/dashboard/dashboard.module.ts
@@ -3,11 +3,14 @@ import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import {LookupService} from '../services/lookup/lookup.service';
+import { Ng2GoogleChartsModule } from 'ng2-google-charts';
+
 @NgModule({
   
   imports: [
     CommonModule,
-    DashboardRoutingModule
+    DashboardRoutingModule,
+    Ng2GoogleChartsModule
   ],
   declarations: [DashboardComponent],
   providers:[LookupService]
