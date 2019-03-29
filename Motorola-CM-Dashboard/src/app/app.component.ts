a0f473f1588d607467ac775e186f350de9c8742b
SOFBANGTECH\amandeep <amandeep.singh@sofbang.com>
2019-03-29 22:49:01 +0530

    okta Authentication part

--- Motorola-CM-Dashboard/src/app/app.component.ts
+++ Motorola-CM-Dashboard/src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
 
+import { Router } from "@angular/router";
+import { OktaAuthService } from '@okta/okta-angular';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
   title = 'Motorola-CM-Dashboard';
+  isAuthenticated: boolean;
+  constructor(public oktaAuth: OktaAuthService, public router: Router) {
+    // Subscribe to authentication state changes
+    this.oktaAuth.$authenticationState.subscribe(
+      (isAuthenticated: boolean)  => this.isAuthenticated = isAuthenticated
+    );
+  }
+
+  async ngOnInit() {
+    // Get the authentication state for immediate use
+    this.isAuthenticated = await this.oktaAuth.isAuthenticated();
+    this.oktaAuth.loginRedirect('/home/dashboard');
+  }
+  async logout() {
+    // Terminates the session with Okta and removes current tokens.
+    await this.oktaAuth.logout();
+    this.router.navigateByUrl('/');
+  }
 }
--- Motorola-CM-Dashboard/src/app/app.module.ts
+++ Motorola-CM-Dashboard/src/app/app.module.ts
@@ -4,6 +4,14 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { MainLayoutModule } from './main-layout/main-layout.module';
 import { HttpModule } from '@angular/http';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HttpClientModule } from '@angular/common/http';
+import {
+  OktaAuthGuard,
+  OktaAuthModule,
+  OktaCallbackComponent,
+} from '@okta/okta-angular';
+import { Routes, RouterModule } from '@angular/router';
 // import { ScNewCasesComponent } from './sc-new-cases/sc-new-cases.component';
 // import { EbsCycleTimesComponent } from './ebs-cycle-times/ebs-cycle-times.component';
 //import { AppSidebarComponent } from './app-sidebar/app-sidebar.component';//imp to import to run services
@@ -12,9 +20,35 @@ import { HttpModule } from '@angular/http';
 // import { AppHeaderComponent } from './app-header/app-header.component';
 // import { AppFooterComponent } from './app-footer/app-footer.component';
 
+
+const config = {
+  issuer: 'https://motorolasolutions.okta.com/oauth2/default',
+  redirectUri: 'https://svccontractmetrics-dev.mot-solutions.com/Motorola-CM-Dashboard/home/dashboard/implicit/callback',
+  clientId: '0oaa7ok51xruc9PBy1t7'
+}
+
+export function onAuthRequired({ oktaAuth, router }) {
+  // Redirect the user to your custom login page
+  router.navigate(['/Motorola-CM-Dashboard/home/dashboard']);
+}
+
+const appRoutes: Routes = [
+  {
+    path: 'implicit/callback',
+    
+    component: OktaCallbackComponent,
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    canActivate: [ OktaAuthGuard ],
+  },
+  
+]
 @NgModule({
   declarations: [
     AppComponent,
+    DashboardComponent
     // ScNewCasesComponent,
     // EbsCycleTimesComponent
     //AppSidebarComponent
@@ -23,9 +57,16 @@ import { HttpModule } from '@angular/http';
     BrowserModule,
     AppRoutingModule,
     MainLayoutModule,
-    HttpModule
+    HttpModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes),
+    OktaAuthModule.initAuth(config),
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
+    
 })
+
+
 export class AppModule { }
