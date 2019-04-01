cff8994218331f4ef4e236e43a77acbbcc3785cb
Khushboo D <khushboo.dhanani@sofbang.com>
2019-04-01 14:55:21 -0500

    Okta authentication changes

--- Motorola-CM-Dashboard/src/app/app.module.ts
+++ Motorola-CM-Dashboard/src/app/app.module.ts
@@ -4,6 +4,15 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { MainLayoutModule } from './main-layout/main-layout.module';
 import { HttpModule } from '@angular/http';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HttpClientModule } from '@angular/common/http';
+import {
+  OktaAuthGuard,
+  OktaAuthModule,
+  OktaCallbackComponent,
+} from '@okta/okta-angular';
+import { Routes, RouterModule } from '@angular/router';
+import { MainLayoutComponent } from './main-layout/main-layout.component';
 // import { ScNewCasesComponent } from './sc-new-cases/sc-new-cases.component';
 // import { EbsCycleTimesComponent } from './ebs-cycle-times/ebs-cycle-times.component';
 //import { AppSidebarComponent } from './app-sidebar/app-sidebar.component';//imp to import to run services
@@ -12,9 +21,37 @@ import { HttpModule } from '@angular/http';
 // import { AppHeaderComponent } from './app-header/app-header.component';
 // import { AppFooterComponent } from './app-footer/app-footer.component';
 
+
+const config = {
+  issuer: 'https://motorolasolutions.okta.com/oauth2/default',
+  redirectUri: 'https://svccontractmetrics-dev.mot-solutions.com/Motorola-CM-Dashboard/home/dashboard',
+  clientId: '0oaa7ok51xruc9PBy1t7'
+}
+
+export function onAuthRequired({ oktaAuth, router }) {
+  // Redirect the user to your custom login page
+  router.navigate(['Motorola-CM-Dashboard/home/dashboard']);
+}
+
+const appRoutes: Routes = [
+
+  {
+    path: 'home',
+    component: MainLayoutComponent,
+    canActivate: [ OktaAuthGuard ],
+  },
+  
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    canActivate: [ OktaAuthGuard ],
+  },
+  
+]
 @NgModule({
   declarations: [
     AppComponent,
+    DashboardComponent
     // ScNewCasesComponent,
     // EbsCycleTimesComponent
     //AppSidebarComponent
@@ -23,9 +60,16 @@ import { HttpModule } from '@angular/http';
     BrowserModule,
     AppRoutingModule,
     MainLayoutModule,
-    HttpModule
+    HttpModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes),
+    OktaAuthModule.initAuth(config),
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
+    
 })
+
+
 export class AppModule { }
--- Motorola-CM-Dashboard/src/app/dashboard/dashboard.component.ts
+++ Motorola-CM-Dashboard/src/app/dashboard/dashboard.component.ts
@@ -1,20 +1,60 @@
 import { Component, OnInit } from '@angular/core';
 import { LookupService } from '../services/lookup/lookup.service';
 import { DataHandlerService } from '../services/data-handler/data-handler.service';
+//import { Component } from '@angular/core';
+//import { Component, OnInit } from '@angular/core';
+import { OktaAuthService } from '@okta/okta-angular';
+import { Router, NavigationStart} from '@angular/router';
+import * as OktaSignIn from '@okta/okta-signin-widget';
+import { logging } from 'protractor';
+
+
+interface ResourceServerExample {
+  label: String;
+  url: String;
+}
 @Component({
-  selector: 'app-dashboard',
+   
+  selector: 'app-dashboard', 
+  template: `
+    <!-- Container to inject the Sign-In Widget -->
+    <div id="okta-signin-container"></div>
+  `,
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-  constructor(private _lookupService: LookupService,private _dataHandlerService: DataHandlerService) {
+  signIn;
+  widget = new OktaSignIn({
+    baseUrl: 'https://{motorolasolutions.okta.com}'
+  });
+  constructor(public oktaAuth: OktaAuthService,private _lookupService: LookupService,private _dataHandlerService: DataHandlerService) {
     this._dataHandlerService.setDataForMainLayout(false);
+    this.signIn = oktaAuth;
    }
    linkClick(link){
      this._dataHandlerService.clickDashboardLink(link);
      console.log("click");
    }
-  ngOnInit() {
+   login() {
+    this.oktaAuth.loginRedirect()
+  }
+   async ngOnInit() { 
+    this.widget.renderEl({
+      el: '#okta-signin-container'},
+      (res) => {
+        if (res.status === 'SUCCESS') {
+          this.signIn.loginRedirect('/', { sessionToken: res.session.token });
+          // Hide the widget
+          this.widget.hide();
+        }
+      },
+      (err) => {
+        throw err;
+      }
+    );
+    
+  
   
     // demo test service for starting the Project
     // this._lookupService.getLookup()
@@ -23,5 +63,4 @@ export class DashboardComponent implements OnInit {
     //   });
 
   }
-
 }
